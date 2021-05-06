c400a924eac5a395d0e8447a21d108d31188fb85
Grant Kayes <glk25@georgetown.edu>
2021-05-05 19:03:04 -0700

    create Home, Mix, Mixes components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import Home from './components/Home';
 import './App.css';
-import fetch from 'isomorphic-fetch';
-import runtimeEnv from '@mars/heroku-js-runtime-env';
 
 function App () {
-  const [ data, setData ] = useState();
-
-  useEffect(() => {
-    const url = runtimeEnv().REACT_APP_API_URL
-    fetch(url)
-      .then( res => res.json() )
-      .then( json => setData(json) );
-  }, []);
-
   return (
-    <p>Message: { data }</p>
+    <Home/>
   );
 }
 

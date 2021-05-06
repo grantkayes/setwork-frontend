2ce4704dec4e851eb37d6fc0605cd1886bb09b6c
Grant Kayes <glk25@georgetown.edu>
2021-05-05 17:22:23 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-
-import fetch from 'isomorphic-fetch'
-import runtimeEnv from '@mars/heroku-js-runtime-env'
+import fetch from 'isomorphic-fetch';
+import runtimeEnv from '@mars/heroku-js-runtime-env';
 
 function App () {
   const [ data, setData ] = useState();
@@ -11,10 +10,8 @@ function App () {
     const url = runtimeEnv().REACT_APP_API_URL
     fetch(url)
       .then( res => res.json() )
-      .then( json => console.log(json))
-      // .then( res => res.json() )
-      // .then( json => setData({ data: json }) )
-  }, [])
+      .then( json => setData(json) );
+  }, []);
 
   return (
     <p>Message: { data }</p>
